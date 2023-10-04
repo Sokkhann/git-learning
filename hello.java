c149e5dbd715535fb07817e2e595f474f5a04179
sokkhann <mrrkhann9@gmail.com>
2023-10-04 15:40:33 +0700

    add hello1.html to git

---
+++ hello.java
@@ -0,0 +1,10 @@
+class Hello {
+    /**
+     * @param args
+     */
+    public static void main (String[] args ) {
+        public void msg() {
+            System.out.println("Hello");
+        }
+    }
+}
